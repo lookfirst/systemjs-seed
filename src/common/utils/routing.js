fe628437ce203b579422c7fc9f1a6806622a60da
Jon Stevens <latchkey@gmail.com>
2015-02-13 01:42:04 -0800

    fix issue with ngannotate not doing its job. sigh.

--- src/common/utils/routing.js
+++ src/common/utils/routing.js
@@ -1,38 +1,37 @@
 import futureRoutes from 'app/routes.json!';
 import 'ui-router-extras';
 
-export var routing = function(module){
+export var routing = function(module) {
 
   module.requires.push('ct.ui.router.extras.future');
   
-  var $inject = ['$stateProvider', '$futureStateProvider'];
   var RouterConfig = function ($stateProvider, $futureStateProvider) {
 
-    $futureStateProvider.stateFactory('load', function($q, $ocLazyLoad, futureState){
+    $futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState', function($q, $ocLazyLoad, futureState) {
       var def = $q.defer();
 
       System.import(futureState.src).then(loaded => {
         var newModule = loaded;
-        if(!loaded.name){
+        if (!loaded.name) {
           var key = Object.keys(loaded);
           newModule = loaded[key[0]];
         }
         
-        $ocLazyLoad.load(newModule).then(function(){
+        $ocLazyLoad.load(newModule).then(function() {
           def.resolve();
         });
       });
 
       return def.promise;
-    });
+    }]);
 
-    futureRoutes.forEach(function(r){
+    futureRoutes.forEach(function(r) {
       $futureStateProvider.futureState(r);
     });
-    
+
   };
 
-  RouterConfig.$inject = $inject;
+  RouterConfig.$inject = ['$stateProvider', '$futureStateProvider'];
 
   return RouterConfig;
 };
