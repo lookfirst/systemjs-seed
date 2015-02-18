8a254579684d48040c7e7380876406e087a767f0
Jon Stevens <latchkey@gmail.com>
2015-02-17 18:21:54 -0800

    refer to the templates by name now

--- src/app/admin/users/users.js
+++ src/app/admin/users/users.js
@@ -3,10 +3,10 @@ import {modalModule} from 'common/components/modal';
 import {selectModule} from 'common/components/select';
 import {dateModule} from 'common/components/date';
 import {timeModule} from 'common/components/time';
-import './users.tpl';
+import usersTpl from 'app/admin/users/users.tpl';
 
 export var usersModule = angular.module('admin.users',
-  [modalModule.name, selectModule.name, dateModule.name, timeModule.name, 'app/admin/users/users.tpl.html']);
+  [modalModule.name, selectModule.name, dateModule.name, timeModule.name, usersTpl.name]);
 
 usersModule.controller('UsersController', function(){
   console.log('users!');
--- src/app/login/login.js
+++ src/app/login/login.js
@@ -3,13 +3,13 @@ import {selectModule} from 'common/components/select';
 import {dateModule} from 'common/components/date';
 import {modalModule} from 'common/components/modal';
 import {userModule} from 'common/services/user';
-import './login.tpl';
-import './signup.tpl';
+import loginTpl from 'app/login/login.tpl';
+import signupTpl from 'app/login/signup.tpl';
 import './login.css!';
 
 
 export var loginModule = angular.module('login', [selectModule.name, dateModule.name, modalModule.name, userModule.name,
-  'app/login/login.tpl.html', 'app/login/signup.tpl.html']);
+  loginTpl.name, signupTpl.name]);
 
 loginModule.config(function($stateProvider){
   $stateProvider.state('login', {
