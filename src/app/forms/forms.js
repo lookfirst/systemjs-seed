62f7dced8f3b384cc25d61ea00c97ad8b2cbca3f
Jon Stevens <latchkey@gmail.com>
2015-02-19 17:24:10 -0800

    fix joshing errors

--- src/app/forms/forms.js
+++ src/app/forms/forms.js
@@ -1,19 +1,19 @@
-define(['angular', 'app/forms/forms.tpl', 'common/components/modal', 'common/components/select'], function(angular, formsTpl){
+define(['angular', 'app/forms/forms.tpl', 'common/components/modal', 'common/components/select'],
+  function(angular, formsTpl) {
+    var formModule = angular.module('forms', [formsTpl.name, 'common.components.modal', 'common.components.select']);
 
-  var formModule = angular.module('forms', [formsTpl.name, 'common.components.modal', 'common.components.select']);
-
-  formModule.config(function($stateProvider){
-    $stateProvider.state('forms', {
-      url: '/forms',
-      templateUrl: 'app/forms/forms.tpl.html',
-      controller: 'FormsCtrl'
+    formModule.config(function($stateProvider) {
+      $stateProvider.state('forms', {
+        url: '/forms',
+        templateUrl: 'app/forms/forms.tpl.html',
+        controller: 'FormsCtrl'
+      });
     });
-  });
 
-  formModule.controller('FormsCtrl', () => {
-      console.log('forms!');
-  });
+    formModule.controller('FormsCtrl', () => {
+        console.log('forms!');
+    });
 
-  return formModule;
-
-});
+    return formModule;
+  }
+);
--- src/common/utils/routing.js
+++ src/common/utils/routing.js
@@ -7,23 +7,25 @@ export var routing = function(module) {
   
   var RouterConfig = function ($stateProvider, $futureStateProvider) {
 
-    $futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState', function($q, $ocLazyLoad, futureState) {
-      var def = $q.defer();
+    $futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState',
+      function($q, $ocLazyLoad, futureState) {
+        var def = $q.defer();
 
-      System.import(futureState.src).then(loaded => {
-        var newModule = loaded;
-        if (!loaded.name) {
-          var key = Object.keys(loaded);
-          newModule = loaded[key[0]];
-        }
-        
-        $ocLazyLoad.load(newModule).then(function() {
-          def.resolve();
+        System.import(futureState.src).then(loaded => {
+          var newModule = loaded;
+          if (!loaded.name) {
+            var key = Object.keys(loaded);
+            newModule = loaded[key[0]];
+          }
+
+          $ocLazyLoad.load(newModule).then(function() {
+            def.resolve();
+          });
         });
-      });
 
-      return def.promise;
-    }]);
+        return def.promise;
+      }
+    ]);
 
     futureRoutes.forEach(function(r) {
       $futureStateProvider.futureState(r);
