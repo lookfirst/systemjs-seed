c4dede3fce7fe202da60a4111ccdfc60465c9232
Jon Stevens <latchkey@gmail.com>
2015-02-26 12:52:11 -0800

    take advantage of the module name now

--- src/app/forms/forms.js
+++ src/app/forms/forms.js
@@ -1,6 +1,8 @@
+'use strict';
+
 define(['angular', 'app/forms/forms.tpl', 'common/components/modal', 'common/components/select'],
-	function(angular, formsTpl) {
-		var formModule = angular.module('forms', [formsTpl.name, 'common.components.modal', 'common.components.select']);
+	function(angular, formsTpl, modal, select) {
+		var formModule = angular.module('forms', [formsTpl.name, modal.name, select.name]);
 
 		formModule.config(function($stateProvider) {
 			$stateProvider.state('forms', {
