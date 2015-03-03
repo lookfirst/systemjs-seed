2c34922fa2eb6cf1bee5757ffec76544fefb7ffe
Jon Stevens <latchkey@gmail.com>
2015-03-03 03:46:40 -0800

    better implementation of the todoFocus directive

--- test/app/todo/directives/directives.spec.js
+++ test/app/todo/directives/directives.spec.js
@@ -4,27 +4,31 @@ import 'angular-mocks';
 
 beforeEach(angular.mock.module(todoModule.name));
 
-describe('todoFocus directive', function () {
-	var $scope, $compile, $browser;
+describe('todoFocus directive', () => {
+	var $scope, $compile, $timeout;
 
-	beforeEach(inject(function (_$rootScope_, _$compile_, _$browser_) {
+	beforeEach(inject((_$rootScope_, _$compile_, _$timeout_) => {
 		let $rootScope = _$rootScope_;
 		$compile = _$compile_;
-		$browser = _$browser_;
+		$timeout = _$timeout_;
+
 		$scope = $rootScope.$new();
 	}));
 
-	it('should focus on truthy expression', function () {
+	it('should focus on truthy expression', () => {
 		var el = angular.element('<input todo-focus="focus">');
+
+		spyOn(el[0], 'focus');
+
 		$scope.focus = false;
-
 		$compile(el)($scope);
-		expect($browser.deferredFns.length).toBe(0);
 
-		$scope.$apply(function () {
+		$scope.$apply(() => {
 			$scope.focus = true;
 		});
 
-		expect($browser.deferredFns.length).toBe(1);
+		$timeout.flush();
+
+		expect(el[0].focus).toHaveBeenCalled();
 	});
 });
