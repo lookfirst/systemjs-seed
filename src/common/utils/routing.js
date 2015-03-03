4fb61d77db993a1c0ca2830292b01f1a6da9ab74
Jon Stevens <latchkey@gmail.com>
2015-03-03 03:46:05 -0800

    need to load $ocLazyLoadProvider in order to properly init oc.lazyload
    formatting cleanup

--- src/common/utils/routing.js
+++ src/common/utils/routing.js
@@ -12,27 +12,28 @@ export default function(module) {
 	module.requires.push('ct.ui.router.extras.core');
 	module.requires.push('ct.ui.router.extras.future');
 
-	var RouterConfig = ['$stateProvider', '$futureStateProvider', function($stateProvider, $futureStateProvider) {
+	var RouterConfig = ['$ocLazyLoadProvider', '$stateProvider', '$futureStateProvider',
+		function($ocLazyLoadProvider, $stateProvider, $futureStateProvider) {
 
 		$futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState',
 			function($q, $ocLazyLoad, futureState) {
-				var def = $q.defer();
 
-				System.import(futureState.src).then(loaded => {
-					var newModule = loaded;
-					if (!loaded.name) {
-						var key = Object.keys(loaded);
-						newModule = loaded[key[0]];
-					}
+			var def = $q.defer();
 
-					$ocLazyLoad.load(newModule).then(function() {
-						def.resolve();
-					});
+			System.import(futureState.src).then(loaded => {
+				var newModule = loaded;
+				if (!loaded.name) {
+					var key = Object.keys(loaded);
+					newModule = loaded[key[0]];
+				}
+
+				$ocLazyLoad.load(newModule).then(function() {
+					def.resolve();
 				});
+			});
 
-				return def.promise;
-			}
-		]);
+			return def.promise;
+		}]);
 
 		futureRoutes.forEach(function(r) {
 			$futureStateProvider.futureState(r);
