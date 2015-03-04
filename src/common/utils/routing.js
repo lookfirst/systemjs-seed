83b4ab9582d82cc386c46899ad6293290dc13b34
Jon Stevens <latchkey@gmail.com>
2015-03-04 11:06:21 -0800

    formatting

--- src/common/utils/routing.js
+++ src/common/utils/routing.js
@@ -1,16 +1,16 @@
 import futureRoutes from 'app/routes.json!';
 
-import 'oclazyload';
 import 'angular-ui-router';
 import 'ui-router-extras/release/modular/ct-ui-router-extras.core';
 import 'ui-router-extras/release/modular/ct-ui-router-extras.future';
+import 'oclazyload';
 
 export default function(module) {
 
-	module.requires.push('oc.lazyLoad');
 	module.requires.push('ui.router');
 	module.requires.push('ct.ui.router.extras.core');
 	module.requires.push('ct.ui.router.extras.future');
+	module.requires.push('oc.lazyLoad');
 
 	var RouterConfig = ['$ocLazyLoadProvider', '$stateProvider', '$futureStateProvider',
 		function($ocLazyLoadProvider, $stateProvider, $futureStateProvider) {
@@ -18,28 +18,29 @@ export default function(module) {
 			$futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState',
 				function($q, $ocLazyLoad, futureState) {
 
-				var def = $q.defer();
+					var def = $q.defer();
 
-				System.import(futureState.src).then(loaded => {
-					var newModule = loaded;
-					if (!loaded.name) {
-						var key = Object.keys(loaded);
-						newModule = loaded[key[0]];
-					}
+					System.import(futureState.src).then(loaded => {
+						var newModule = loaded;
+						if (!loaded.name) {
+							var key = Object.keys(loaded);
+							newModule = loaded[key[0]];
+						}
 
-					$ocLazyLoad.load(newModule).then(function() {
-						def.resolve();
+						$ocLazyLoad.load(newModule).then(function() {
+							def.resolve();
+						});
 					});
-				});
 
-				return def.promise;
-			}]);
+					return def.promise;
+				}
+			]);
 
 			futureRoutes.forEach(function(r) {
 				$futureStateProvider.futureState(r);
 			});
-
-	}];
+		}
+	];
 
 	return RouterConfig;
 }
