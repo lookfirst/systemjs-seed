5492167364a5c72c245cb06439b4649469497771
Jon Stevens <latchkey@gmail.com>
2015-03-05 02:29:33 -0800

    use verbose output

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'verbose', 'coverage'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
