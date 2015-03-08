6d282c9e280fd0ff61d078b24e36750a10319f77
Jon Stevens <latchkey@gmail.com>
2015-03-08 12:52:22 -0700

    forgot a command

--- .travis.chrome.sh
+++ .travis.chrome.sh
@@ -2,6 +2,7 @@
 
 # Holy shit batman! https://github.com/travis-ci/travis-ci/issues/938
 
+sudo apt-get install -y python-software-properties
 sudo apt-add-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
 sudo sed -i s/deb-src.*google.*//g /etc/apt/sources.list
 wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
