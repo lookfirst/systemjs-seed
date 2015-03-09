172c5f0d41fec4cf7556052d7a8709ec5ab6c013
Jon Stevens <latchkey@gmail.com>
2015-03-08 20:06:13 -0700

    no sudo, boo

--- .travis.chrome.sh
+++ .travis.chrome.sh
@@ -6,7 +6,7 @@
 
 apt-get install -y python-software-properties
 apt-add-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
-sudo sed -i s/deb-src.*google.*//g /etc/apt/sources.list
+sed -i s/deb-src.*google.*//g /etc/apt/sources.list
 wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 apt-get update -qq
 apt-get install -y google-chrome-stable
