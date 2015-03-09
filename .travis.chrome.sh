951ba33b1a8177d5706fdf4cba7c56bcb1c6d124
Jon Stevens <latchkey@gmail.com>
2015-03-08 22:54:50 -0700

    sudo!

--- .travis.chrome.sh
+++ .travis.chrome.sh
@@ -4,11 +4,11 @@
 # https://github.com/travis-ci/travis-ci/issues/938
 # https://github.com/travis-ci/travis-ci/issues/2555
 
-apt-get install -y python-software-properties
-apt-add-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
-sed -i s/deb-src.*google.*//g /etc/apt/sources.list
-wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-apt-get update -qq
-apt-get install -y google-chrome-stable
+sudo apt-get install -y python-software-properties
+sudo apt-add-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo sed -i s/deb-src.*google.*//g /etc/apt/sources.list
+wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get install -y google-chrome-stable
 
-chmod 1777 /dev/shm
+sudo chmod 1777 /dev/shm
