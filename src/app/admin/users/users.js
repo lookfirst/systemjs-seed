9fa2dfc0bd693dd33b731b31ae6b5d53ef1a7021
Jon Stevens <latchkey@gmail.com>
2015-03-20 18:13:57 -0700

    fix export of usersModule

--- src/app/admin/users/users.js
+++ src/app/admin/users/users.js
@@ -5,9 +5,11 @@ import dateModule from 'common/components/date';
 import timeModule from 'common/components/time';
 import usersTpl from 'app/admin/users/users.tpl';
 
-export var usersModule = angular.module('admin.users',
+var usersModule = angular.module('admin.users',
 	[modalModule.name, selectModule.name, dateModule.name, timeModule.name, usersTpl.name]);
 
 usersModule.controller('UsersController', function() {
 	console.log('users!');
 });
+
+export default usersModule;
